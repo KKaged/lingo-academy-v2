bcfd97591f2e30bf07a647259f5fff4e9fe1cb22
KKaged <dhigaredaleon707@gmail.com>
2024-10-23 11:38:32 +0900

    edited meta data to japanese, added keywords

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,12 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { NextUIProvider } from "@nextui-org/react";
 
-
-
 export const metadata: Metadata = {
-  title: "Lingo-Academy",
-  description: "A nexus of learning english",
+  title: "Lingo Academy",
+  description:
+    "横浜の英会話スクール「Lingo Academy」で、楽しく効果的に英語を学びませんか？初心者から上級者まで対応した魅力的な英語レッスンとサポートが充実したコミュニティで、あなたの目標に合わせたパーソナライズされたクラスを提供します。フレキシブルなスケジュールで、実践的な会話練習や文化交流を体験し、英語力を向上させましょう。フレンドリーな環境で英語を学ぶ楽しさを見つけましょう—今すぐお申し込みください！",
+  keywords:
+    "英会話スクール, 横浜, Lingo Academy, 英語を学ぶ, 初心者, 上級者, 実践的な会話練習, 英語力向上",
 };
 
 export default function RootLayout({
@@ -17,10 +18,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <NextUIProvider>
-        {children}
-        </NextUIProvider>
-        </body>
+        <NextUIProvider>{children}</NextUIProvider>
+      </body>
     </html>
   );
 }
